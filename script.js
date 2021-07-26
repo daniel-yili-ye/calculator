dcf3bd0c67342558ac9bd700a29a2a2a7e61fb75
Daniel Ye <daniel.ye@live.com>
2021-07-26 00:50:08 -0400

    changes

--- script.js
+++ script.js
@@ -3,16 +3,31 @@ const text = document.querySelector("input")
 const del = document.querySelector("button[id='DEL']")
 const ce = document.querySelector("button[id='CE']")
 let eq = ""
+let flag = true
+const rx = /[\+\-\*\/]/
 
 btns.forEach(btn => btn.addEventListener("click", e => {
-    if (!isNaN(e.toElement.innerHTML)) {
-        text.value += e.toElement.innerHTML
+    if (!isNaN(e.toElement.innerHTML)) { // if its a valid number OR first decimal place
+        if (flag) {
+            text.value += e.toElement.innerHTML
+        }
+        else {
+            text.value = e.toElement.innerHTML
+            flag = true
+        }
+        eq += e.toElement.innerHTML
     }
-    else if (/[+-*/=]/.test(e.toElement.innerHTML)) {
-        evaluate(eq)
+    // exception for = sign
+    else if (e.toElement.innerHTML == "=") {
+        text.value = evaluate(eq)
+        flag = false
+    }
+    else if (rx.test(text.value + e.toElement.innerHTML)) { // if there are 2 signs
+        eq = text.value + e.toElement.innerHTML
+        text.value = ""
     }
     else {
-        eq += text.value + e.toElement.innerHTML
+        eq += e.toElement.innerHTML
     }
 }))
 
@@ -25,5 +40,5 @@ ce.addEventListener("click", e => {
 })
 
 function evaluate(y) {
-    math.evaluate(y)
+    return (math.evaluate(y)).toString()
 }
