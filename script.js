a1e48e931dd4754f00c81dd8e1a406d68216aabd
Daniel Ye <daniel.ye@live.com>
2021-07-26 15:52:27 -0400

    bugfixes

--- script.js
+++ script.js
@@ -3,42 +3,50 @@ const text = document.querySelector("input")
 const del = document.querySelector("button[id='DEL']")
 const ce = document.querySelector("button[id='CE']")
 let eq = ""
-let flag = true
+let overwrite = false
 const rx = /[\+\-\*\/]/
 
 btns.forEach(btn => btn.addEventListener("click", e => {
-    if (!isNaN(e.toElement.innerHTML)) { // if its a valid number OR first decimal place
-        if (flag) {
-            text.value += e.toElement.innerHTML
-        }
-        else {
+    if (!isNaN(e.toElement.innerHTML)) { // if its a valid number
+        if (overwrite) { // overwrite -> need to make another overwrite condition where you overwrite text.value and eq (essentially hitting CE button)
             text.value = e.toElement.innerHTML
-            flag = true
+            overwrite = false
         }
-        eq += e.toElement.innerHTML
+        else { // append
+            text.value += e.toElement.innerHTML
+        } 
+        eq += e.toElement.innerHTML // number or decimal point is added onto equation
     }
-    // exception for = sign
-    else if (e.toElement.innerHTML == "=") {
+    else if (e.toElement.innerHTML == "=") { // exception for = sign
         text.value = evaluate(eq)
-        flag = false
+        eq = text.value
+        overwrite = true
     }
-    else if (rx.test(text.value + e.toElement.innerHTML)) { // if there are 2 signs
+    else if (rx.test(e.toElement.innerHTML)) { // if there is 1 signs
+        text.value = evaluate(eq)
         eq = text.value + e.toElement.innerHTML
-        text.value = ""
+        overwrite = true
     }
-    else {
+    else if (e.toElement.innerHTML == "." && !eq.includes(".")) { // first decimal place
+        text.value += e.toElement.innerHTML
         eq += e.toElement.innerHTML
     }
 }))
 
-del.addEventListener("click", e => {
+del.addEventListener("click", () => {
     text.value = text.value.substr(0, text.value.length - 1)
+    eq = eq.substr(0, eq.length - 1)
 })
 
-ce.addEventListener("click", e => {
-    text.value = ""
+ce.addEventListener("click", () => {
+    text.value, eq = ""
 })
 
 function evaluate(y) {
-    return (math.evaluate(y)).toString()
+    try {
+        return (math.evaluate(y)).toString()
+    }
+    catch(err) {
+        return "Syntax Error"
+    }
 }
